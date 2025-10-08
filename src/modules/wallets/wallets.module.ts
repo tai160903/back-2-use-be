fb33eacd31d9581df2536cb8d7c87908f9a3a414
tai160903 <ltt16092003@gmail.com>
2025-10-08 17:47:41 +0700

    add vnpay sandbox

--- src/modules/wallets/wallets.module.ts
+++ src/modules/wallets/wallets.module.ts
@@ -3,10 +3,12 @@ import { WalletsService } from './wallets.service';
 import { WalletsController } from './wallets.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Wallets, WalletsSchema } from './schemas/wallets.schema';
+import { VnpayModule } from '../vnpay/vnpay.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Wallets.name, schema: WalletsSchema }]),
+    VnpayModule,
   ],
   controllers: [WalletsController],
   providers: [WalletsService],
